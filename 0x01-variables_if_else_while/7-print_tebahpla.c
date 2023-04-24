65fec1dd181320ed9df9253d05d17fbf9fb26698
ednavalentine <valentineedna@gmail.com>
2023-04-24 15:41:49 -0700

    added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry pont
+ * Description: 'extracting lowercase in reverse'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 25; p >= 0; p++)
+	{
+		putchar('a' + p);
+	}
+	putchar('\n');
+	return (0);
+}
