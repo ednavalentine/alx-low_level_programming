30f140ea46c0b14c7e73e6142859971a004e86f4
ednavalentine <valentineedna@gmail.com>
2023-04-24 15:29:04 -0700

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Prints all single digits without using char'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
