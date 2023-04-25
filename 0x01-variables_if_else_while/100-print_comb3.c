62f1b9d2df1fdd213a8e2d9ad3eb19532e73ba27
ednavalentine <valentineedna@gmail.com>
2023-04-24 17:37:03 -0700

    made changes to 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,17 @@ int main(void)
 
 	for (z1 = 0; z1 < 9; z1++)
 	{
-	for (z1 = z2 + 1; z2 < 10; z2++)
-	{
-		putchar((z1 % 10) + '0');
-		putchar((z2 % 10) + '0');
-
-		if (z1 == 8 && z2 == 9)
-			continue;
+		z2 = z1 + 1;
+		do {
+		putchar(z1 + '0');
+		putchar(z2 + '0');
+		if (z1 < 8)
+		{
 		putchar(',');
-		putchar(' ');
-	}
+		putchar(32);
+		}
+		z2++;
+		} while (z2 < 10);
 	}
 	putchar('\n');
 	return (0);
