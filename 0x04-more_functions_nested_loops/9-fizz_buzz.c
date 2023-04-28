fe01a48e6516092bcb981f20c0d9afbd6b301bb9
ednavalentine <valentineedna@gmail.com>
2023-04-28 08:53:42 -0700

    updated 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,27 @@
  */
 int main(void)
 {
-	int i;
+	int h = 1;
 
-	for (i = 1; i <= 100; i++)
+	while (h <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
+		if (h % 3 == 0 && h % 5 == 0)
 			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
+		else if (h % 5 == 0)
 		{
+			if (h == 100)
+			{
+				printf("Buzz");
+				printf("\n");
+			}
+			else
+				printf("Buzz ");
+		}
+		else if (h % 3 == 0)
 			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
 		else
-		{
-			printf("%d ", i);
-		}
+			printf("%d ", h);
+	h++;
 	}
-	printf("\n");
 	return (0);
 }
