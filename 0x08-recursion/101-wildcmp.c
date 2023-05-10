28ff0c5a4ace71190ef2414487228077f0a91041
ednavalentine <valentineedna@gmail.com>
2023-05-10 05:16:37 -0700

    updated 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,17 +13,21 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s2 == '*')
 	{
-		if (*(s2+1) == '\0')
+		if (*(s2 + 1) == '\0')
 		{
 			return (1);
 		}
+		else if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
 		else
 		{
-			return ((*s1 == '\0') ? wildcmp(s1, s2+1) : (wildcmp(s1, s2+1) || wildcmp(s1+1, s2)));
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 		}
 	}
 	else
 	{
-	return ((*s1 == *s2) ? wildcmp(s1+1, s2+1) : 0);
+	return ((*s1 == *s2) ? wildcmp(s1 + 1, s2 + 1) : 0);
 	}
 }
