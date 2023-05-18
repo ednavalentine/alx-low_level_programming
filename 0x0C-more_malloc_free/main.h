1ba6dd7f1ef470cb10093af54b9da5523887ef0b
ednavalentine <valentineedna@gmail.com>
2023-05-18 09:06:10 -0700

    made changes to 101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int multiply(int num1, int num2);
+int isNumber(const char* str);
+void printNumber(int number);
+int main(int argc, char* argv[]);
 
 #endif /* MAIN_H */
