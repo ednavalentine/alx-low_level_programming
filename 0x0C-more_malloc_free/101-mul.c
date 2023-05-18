623d04f22b8284fc7069599764f4c84100802956
ednavalentine <valentineedna@gmail.com>
2023-05-18 11:22:14 -0700

    changes made in 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -52,43 +52,43 @@ void error(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int leng, leng1, leng2, ink, cat, line1, line2, *results, ant = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		error();
-	leng1 =  _strlen(s1);
-	leng2 =  _strlen(s2);
-	leng = leng1 + leng2 + 1;
-	results = malloc(sizeof(int) * leng);
-	if (!results)
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
 		return (1);
-	for (ink = 0; ink <= leng1 + leng2; ink++)
-		results[ink] = 0;
-	for (leng1 = leng1 - 1; leng1 >= 0; leng1--)
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		line1 = s1[leng1] - 0;
-		cat = 0;
-		for (leng2 = _strlen(s2) - 1; leng2 >= 0; leng2--)
-		{
-			line2 = s2[leng2] - '0';
-			cat += results[leng1 + leng2 + 1] + (line1 * line2);
-			results[leng1 + leng2 + 1] = cat % 10;
-			cat /= 10;
-		}
-		if (cat > 0)
-			results[leng1 + leng2 + 1] += cat;
+		digit1 = s1[len1] - '0';
+		carry = 0;
+	for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+	{
+		digit2 = s2[len2] - '0';
+		carry += result[len1 + len2 + 1] + (digit1 * digit2);
+		result[len1 + len2 + 1] = carry % 10;
+		carry /= 10;
 	}
-		for (ink = 0; ink < leng - 1; ink++)
-		{
-			if (results[ink])
-				ant = 1;
-			if (ant)
-				_putchar(results[ink] + '0');
-		}
-		if (!ant)
-			_putchar('0');
-		_putchar('\n');
-		free(results);
-		return (0);
+	if (carry > 0)
+		result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
