cc51d58810936e285e57522d0363cf5d48019e38
ednavalentine <valentineedna@gmail.com>
2023-05-23 17:45:37 +0300

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - shows a dog with name, age & owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owners name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
