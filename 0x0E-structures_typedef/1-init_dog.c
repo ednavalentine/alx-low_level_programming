23a2729cb5e7d601a253ab150f14f633d756d0b4
ednavalentine <valentineedna@gmail.com>
2023-05-23 21:52:27 +0300

    updated 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -13,15 +12,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = malloc(strlen(name) + 1);
-	if (d->name != NULL)
-	{
-		strcpy(d->name, name);
-	}
+	if (d == NULL)
+		return;
+	d->name = name;
 	d->age = age;
-	d->owner = malloc(strlen(owner) + 1);
-	if (d->owner != NULL)
-	{
-		strcpy(d->owner, owner);
-	}
+	d->owner = owner;
 }
