d2745fb1dc80be48f8f97d41aa81ee12bd4b3cbf
ednavalentine <valentineedna@gmail.com>
2023-05-24 17:35:27 +0300

    made changes to 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,26 +8,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int num_bytes, i;
+	int num_bytes = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	num_bytes = atoi(argv[1]);
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < num_bytes; i++)
+	while (num_bytes < atoi(argv[1]))
 	{
-		printf("%02hhx", *((char *)main + 1));
-		if (i < num_bytes - 1)
+		printf("%02x", *((unsigned char *)main + num_bytes));
+		num_bytes++;
+		if (atoi(argv[1]) > num_bytes)
+		{
 			printf(" ");
-		else
-			printf("\n");
+		}
 	}
+	printf("\n");
 	return (0);
 }
