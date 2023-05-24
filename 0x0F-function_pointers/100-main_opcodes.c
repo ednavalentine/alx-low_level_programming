fa66e55974cfe336d5e4f5258e320266a7226393
ednavalentine <valentineedna@gmail.com>
2023-05-24 16:32:10 +0300

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,32 +10,26 @@ void print_opcodes(int num_bytes);
  */
 int main(int argc, char *argv[])
 {
-	int num_bytes;
+	int i, num_bytes;
+	void *main_addr;
+	unsigned char *opcodes;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 	num_bytes = atoi(argv[1]);
 	if (num_bytes <= 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
-	print_opcodes(num_bytes);
-	return (0);
-}
-
-void print_opcodes(int num_bytes)
-{
-	unsigned char *opcode_ptr;
-	int ink;
-
-	opcode_ptr = (unsigned char *)main;
-	for (ink = 0; ink < num_bytes; ink++)
+	main_addr = (void *)main;
+	opcodes = (unsigned char *)main_addr;
+	for (i = 0; i < num_bytes; i++)
 	{
-		printf("%02x ", opcode_ptr[ink]);
+		printf("%02x ", opcodes[i]);
 	}
 	printf("\n");
 }
