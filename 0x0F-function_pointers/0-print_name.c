285fcf0dbfc10ad9a531be96ec93e24f58ac04a2
ednavalentine <valentineedna@gmail.com>
2023-05-24 13:02:25 +0300

    added 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to char
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	f(name);
+}
