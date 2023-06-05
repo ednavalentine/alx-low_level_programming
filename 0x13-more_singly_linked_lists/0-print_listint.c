b77810c2e33487c4c2a5966756d73068fca39dad
ednavalentine <valentineedna@gmail.com>
2023-06-05 20:58:36 +0300

    made changes to 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,16 +14,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n != 0)
-		{
-			printf("%d\n", h->n);
-		}
-		else
-		{
-			printf ("0\n");
-		}
-		ink++;
+		printf("%d\n", h->n);
 		h = h->next;
+		ink++;
 	}
 	return (ink);
 }
