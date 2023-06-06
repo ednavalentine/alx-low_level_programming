305fac79973b6f984655b1b96c9c0d5caee22834
ednavalentine <valentineedna@gmail.com>
2023-06-06 20:02:28 +0300

    updated 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * _loop - reallocates memory of arrays
+ * _l - reallocates memory of arrays
  * @list: list to be reallocated
- * @sizer: new size of list
- * @new: new node to be added to the list
+ * @size: new size of list
+ * @now: new node to be added to the list
  * Return: pointer to list
  */
-const listint_t **_loop(const listint_t **list, size_t sizer, const listint_t *now)
+const listint_t **_l(const listint_t **list, size_t size, const listint_t *now)
 {
 	const listint_t **tort;
 	size_t ink;
 
-	tort = malloc(sizer * sizeof(listint_t *));
+	tort = malloc(size * sizeof(listint_t *));
 	if (tort == NULL)
 	{
 		free(list);
 		exit(98);
 	}
-	for (ink = 0; ink < sizer - 1; ink++)
+	for (ink = 0; ink < size - 1; ink++)
 		tort[ink] = list[ink];
 	tort[ink] = now;
 	free(list);
@@ -48,7 +48,7 @@ size_t print_listint_safe(const listint_t *head)
 			}
 		}
 		jum++;
-		ink = _loop(ink, jum, head);
+		ink = _l(ink, jum, head);
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 	}
