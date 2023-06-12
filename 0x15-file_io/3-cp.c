568060a84a310d252f8a10dfed6ac19ccb9721f5
ednavalentine <valentineedna@gmail.com>
2023-06-12 21:20:11 +0300

    made changes to 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ void print_error(const char *message)
  */
 void exit_with_error(const char *message, int exit_code)
 {
-	print_error(message);
+	dprintf(STDERR_FILENO, "%s\n", message);
 	exit(exit_code);
 }
 /**
@@ -43,27 +43,30 @@ int main(int argc, char *argv[])
 	ssize_t ink_read, ink_write;
 
 	if (argc != 3)
-		exit_with_error("Usage: cp file_from file_to", 97);
+		exit_with_error("Usage: cp ink_from ink_to", 97);
 	ink_from = argv[1];
 	ink_to = argv[2];
 	mode_from = open(ink_from, O_RDONLY);
 	if (mode_from == -1)
-		exit_with_error(ink_from, 98);
+		exit_with_error("Error: Can't read from file", 98);
 	mode_to = open(ink_to, O_WRONLY | O_CREAT | O_TRUNC, 
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (mode_to == -1)
-		exit_with_error(ink_to, 99);
+		exit_with_error("Error: Can't write to file", 99);
 	while ((ink_read = read(mode_from, buffer, BUFF)) > 0)
 	{
 		ink_write = write(mode_to, buffer, ink_read);
 		if (ink_write == -1)
-			exit_with_error(ink_to, 99);
+			exit_with_error("Error: Can't write to file", 99);
 	}
 	if (ink_read == -1)
-		exit_with_error(ink_to, 99);
+		exit_with_error("Error: Can't write to file", 99);
 	if (close(mode_from) == -1)
-		exit_with_error("Can't close mode_from", 100);
+		exit_with_error("Error: Can't close fd", 100);
 	if (close(mode_to) == -1)
-		exit_with_error("Can't close mode_from", 100);
+		exit_with_error("Error: Can't close fd", 100);
+	if (chmod(ink_to, S_IRUSR | S_IWUSR 
+			| S_IRGRP | S_IWGRP | S_IROTH) == -1)
+		exit_with_error("Error: Can't set file permissions", 100);
 	return (0);
 }
