1da405f4f340c094c6bfff6090344c880b35d847
ednavalentine <valentineedna@gmail.com>
2023-06-13 00:50:59 +0300

    made changes to 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
 #define BUFF 1024
 
@@ -15,17 +16,7 @@
 void print_error(const char *message)
 {
 	dprintf(STDERR_FILENO, "%s\n", message);
-}
-/**
- * exit_with_error - exits with an error message
- * @message: error message
- * @exit_code: the exit point of the code
- * Return: void
- */
-void exit_with_error(const char *message, int exit_code)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(exit_code);
+	exit(1);
 }
 /**
  * main - main code
@@ -43,30 +34,54 @@ int main(int argc, char *argv[])
 	ssize_t ink_read, ink_write;
 
 	if (argc != 3)
-		exit_with_error("Usage: cp ink_from ink_to", 97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp ink_from ink_to\n");
+		exit(97);
+	}
 	ink_from = argv[1];
 	ink_to = argv[2];
 	mode_from = open(ink_from, O_RDONLY);
 	if (mode_from == -1)
-		exit_with_error("Error: Can't read from file", 98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
+		exit(98);
+	}
 	mode_to = open(ink_to, O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (mode_to == -1)
-		exit_with_error("Error: Can't write to file", 99);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+		exit(99);
+	}
 	while ((ink_read = read(mode_from, buffer, BUFF)) > 0)
 	{
 		ink_write = write(mode_to, buffer, ink_read);
 		if (ink_write == -1)
-			exit_with_error("Error: Can't write to file", 99);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+			exit(99);
+		}
 	}
 	if (ink_read == -1)
-		exit_with_error("Error: Can't write to file", 99);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read to file\n");
+		exit(99);
+	}
 	if (close(mode_from) == -1)
-		exit_with_error("Error: Can't close fd", 100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
+	}
 	if (close(mode_to) == -1)
-		exit_with_error("Error: Can't close fd", 100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
+	}
 	if (chmod(ink_to, S_IRUSR | S_IWUSR
 			| S_IRGRP | S_IWGRP | S_IROTH) == -1)
-		exit_with_error("Error: Can't set file permissions", 100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't set file permissions\n");
+		exit(100);
+	}
 	return (0);
 }
